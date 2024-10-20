15991f3c6ee2dbace8ab4e439e0036dba7b22a7c
Yazid Bintang <yazidbintang2711@gmail.com>
2024-10-20 14:24:18 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/asclepius/view/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.dicoding.asclepius.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.asclepius.R
+
+class SplashActivity : AppCompatActivity() {
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      enableEdgeToEdge()
+      setContentView(R.layout.activity_splash)
+      ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+         val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+         v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+         insets
+      }
+      Handler().postDelayed({
+         startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+      }, 3000)
+   }
+}
